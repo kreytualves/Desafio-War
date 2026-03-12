709c5c836f5c128ee5595d1b42faf875c654839d
kreytualves <kreytualves@gmail.com>
2026-03-12 20:10:29 +0000

    codigo do desafio

--- war novato.c
+++ war novato.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+#include <string.h>
 
+struct Territorio {
+    
+    char nome[30];
+    char cor[10];
+    int tropas;
+};
+
+int main(){
+    struct Territorio territorios[5];
+    
+    for (int i = 0; i < 5; i++){
+        printf("\nCadastro do territorio %d\n", i + 1);
+        
+        printf("Digite o nome do territorio: ");
+        scanf("%s", territorios[i].nome);
+        
+        printf("Digite a cor do exercito: ");
+        scanf("%s", territorios[i].cor);
+        
+        printf("Quantidade de tropas: ");
+        scanf("%d", &territorios[i].tropas);
+    }
+    
+    printf("\n===TERRITORIOS CADASTRADOS===\n");
+    
+    for (int i = 0; i < 5; i++) {
+        printf("\nTerritorio %d\n", i + 1);
+        printf("Nome: %s\n", territorios[i].nome);
+        printf("Cor do exercito: %s\n", territorios[i].cor);
+        printf("Quantidade de tropas: %d\n", territorios[i].tropas);
+    }
+    return 0;
+}
